259e8c02a652b7bf7666273c7aeac62a808bad55
LaloPaniagua <65317268+LaloPaniagua@users.noreply.github.com>
2021-06-23 20:11:15 -0500

    Add files via upload

---
+++ ejercicio2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *array = NULL,contador,num;
+    printf("Cuantos elementos va a tener el arreglo: ");
+    scanf("%d",&num);
+    array = (int*) realloc(array,num*sizeof(int));
+    if(array != NULL){
+        printf("Vector reservado: \n[   ");
+        for(contador = 0; contador<num;contador++){
+            printf("Valor: %d direccion: %p -->",*(array+contador),array+contador);
+        }
+        printf("    ]\n");
+        
+    }
+    num -= 1;
+    array = (int*) realloc(array,num*sizeof(int));
+    if(array != NULL){
+        for(;contador < num;contador++){
+            printf("Ingrese el dato del elemento %d: ",contador);
+            scanf("%d",(array+contador));
+        }
+        printf("Vector reservado: \n[   ");
+        for(contador = 0; contador<num;contador++){
+            printf("Valor: %d direccion: %p -->",*(array+contador),array+contador);
+        }
+        printf("    ]\n");
+        free(array);
+        printf("La memoria ha sido liberada.\n");
+    }  
+    return 0;
+}
