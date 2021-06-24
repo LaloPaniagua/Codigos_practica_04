74adb83470467f07a3c0a37202cedeed0c6ab528
LaloPaniagua <65317268+LaloPaniagua@users.noreply.github.com>
2021-06-24 15:48:56 -0500

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -16,14 +16,16 @@ NODO* crear_nodo_calloc(INFO info){
 int main(){
     INFO a=1;
     INFO b=2;
-    NODO* nodo_m = crear_nodo_malloc(a);
-    NODO* nodo_c = crear_nodo_calloc(b);
+    NODO* nodo_m = crear_nodo_malloc(a); // Nodo creado con malloc
+    NODO* nodo_c = crear_nodo_calloc(b); // Nodo creado con calloc
 
     printf("Direccion de memoria del nodo creado con malloc: %p\n",nodo_m);
     printf("Direccion de memoria del nodo creado con calloc: %p\n",nodo_c);
     getchar();
     return EXIT_SUCCESS;
 }
+
+//Programa creado por Salgado Becerra Justheene Ezequiel y Paniagua Broca Eduardo Miguel el 17/6/2021
 
 
 
